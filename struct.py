85a796b20deddc8cb764ab58848efada9ef9f3dd
Elie Saliba <75352894+elslb@users.noreply.github.com>
2023-10-31 02:02:47 -0400

    Update struct.py

--- struct.py
+++ struct.py
@@ -190,6 +190,176 @@ for day in currentmonthlist:
         if not isindf.empty:
             isinrefassetlist, isinfinaldaylist, isinvallist, isinsettlelist, isinlist, isinnotional, isinmemory, isininv, isinident, isinauto, isintpayingint, isininterest, isinlongname = process_dataframe(isindf, tickerlist)
 
+
+            def process_worksheet(ws, df, refassetlist, vallist, settlelist, list_, notional, memory, inv, ident, auto, tpayingint, interest):
+                """
+                Process the worksheet based on the provided data.
+            
+                Parameters:
+                - ws: The worksheet to process.
+                - df: The dataframe to check if it's empty.
+                - refassetlist, vallist, ... : Lists containing the data to be processed.
+                """
+            
+                if df.empty == False:
+                    # Initialize columns
+                    valdatecol = paydatecol = cusipcol = cusipcolletter = refassetcol = principalcol = None
+                    notecol = memorycol = autocallcol = autocolletter = invcol = identcol = identcolletter = None
+                    payingintcol = interestcol = intcolletter = None
+            
+                    def cellmaker(colletter, rownum, value):
+                        """Helper function to set cell value and alignment."""
+                        ws[rf'{colletter}{rownum}'] = value
+                        ws[rf'{colletter}{rownum}'].alignment = Alignment(horizontal='center', vertical='center')
+            
+                    for i in range(len(refassetlist)):
+                        for cell in ws[1]:
+                            if cell.value:
+                                cell_value = cell.value.strip()
+                                if cell_value == 'Valuation Date':
+                                    valdatecol = cell.column if valdatecol is None else valdatecol
+                                    cellmaker(cell.column_letter, i+2, vallist[i])
+                                elif cell_value == 'Payment Date':
+                                    paydatecol = cell.column if paydatecol is None else paydatecol
+                                    cellmaker(cell.column_letter, i+2, settlelist[i])
+                                elif cell_value == 'ISIN / CUSIP':
+                                    cusipcol = cell.column if cusipcol is None else cusipcol
+                                    cusipcolletter = cell.column_letter if cusipcolletter is None else cusipcolletter
+                                    cellmaker(cell.column_letter, i+2, list_[i])
+                                elif cell_value == 'Reference Asset':
+                                    refassetcol = cell.column if refassetcol is None else refassetcol
+                                    cellmaker(cell.column_letter, i+2, refassetlist[i])
+                                elif cell_value == 'Principal':
+                                    principalcol = cell.column if principalcol is None else principalcol
+                                    cellmaker(cell.column_letter, i+2, abs(notional[i]))
+                                elif cell_value == 'No. of Notes':
+                                    notecol = cell.column if notecol is None else notecol
+                                    cellmaker(cell.column_letter, i+2, abs(notional[i]/1000))
+                                elif cell_value == 'Memory':
+                                    memorycol = cell.column if memorycol is None else memorycol
+                                    cellmaker(cell.column_letter, i+2, memory[i])
+                                elif cell_value == 'Inventory':
+                                    invcol = cell.column if invcol is None else invcol
+                                    cellmaker(cell.column_letter, i+2, inv[i])
+                                elif cell_value == 'Ident':
+                                    identcol = cell.column if identcol is None else identcol
+                                    identcolletter = cell.column_letter if identcolletter is None else identcolletter
+                                    cellmaker(cell.column_letter, i+2, ident[i])
+                                elif cell_value == 'Autocalled':
+                                    autocallcol = cell.column if autocallcol is None else autocallcol
+                                    autocolletter = cell.column_letter if autocolletter is None else autocolletter
+                                    cellmaker(cell.column_letter, i+2, auto[i])
+                                elif cell_value == 'Paying interest?':
+                                    payingintcol = cell.column if payingintcol is None else payingintcol
+                                    cellmaker(cell.column_letter, i+2, tpayingint[i])
+                                elif cell_value == 'Interest':
+                                    interestcol = cell.column if interestcol is None else interestcol
+                                    intcolletter = cell.column_letter if intcolletter is None else intcolletter
+                                    # cellmaker(cell.column_letter, i+2, interest[i])
+
+                    int_col = ref_asset_col = ident_col = None
+                    for col in range(1, ws.max_column+1):
+                        cell_value = ws.cell(row=1, column=col).value
+                        if cell_value == 'Interest':
+                            int_col = col
+                        elif cell_value == 'Reference Asset':
+                            ref_asset_col = col
+                        elif cell_value == 'Ident':
+                            ident_col = col
+            
+                    # fill out 'Interest' Column            
+                    for i in range(1,ws.max_row):
+                        ws.cell(row=i+1, column=int_col).value = f"=AC{i+1}*D{i+1}/12"
+    
+                                                    
+                    # Merging cells
+                    rows_iterate = []
+                    rowcounter = 2
+                    startmergerow = None
+                    for cell in ws[cusipcolletter]: # Only iterates through cusip column
+                        if cell.row == 1:
+                            continue
+                        elif (cell.value is not None) & (cell.value != 1):
+                            if ws[rf'{cell.column_letter}{rowcounter}'].value == ws[rf'{cell.column_letter}{rowcounter+1}'].value:
+                                if startmergerow is not None:
+                                    rowcounter += 1
+                                    continue
+                                else:
+                                    startmergerow = ws[rf'{cell.column_letter}{rowcounter}'].row
+                                    rowcounter += 1
+                                    continue
+                            else:
+                                if startmergerow is None:
+                                    startmergerow = ws[rf'{cell.column_letter}{rowcounter}'].row
+                                endmergerow = ws[rf'{cell.column_letter}{rowcounter}'].row
+                                ws.merge_cells(start_row=startmergerow,start_column=cusipcol,end_row=endmergerow,end_column=cusipcol)
+                                ws.merge_cells(start_row=startmergerow,start_column=valdatecol,end_row=endmergerow,end_column=valdatecol)
+                                ws.merge_cells(start_row=startmergerow,start_column=paydatecol,end_row=endmergerow,end_column=paydatecol)
+                                ws.merge_cells(start_row=startmergerow,start_column=principalcol,end_row=endmergerow,end_column=principalcol)
+                                ws.merge_cells(start_row=startmergerow,start_column=notecol,end_row=endmergerow,end_column=notecol)
+                                ws.merge_cells(start_row=startmergerow,start_column=memorycol,end_row=endmergerow,end_column=memorycol)
+                                ws.merge_cells(start_row=startmergerow,start_column=invcol,end_row=endmergerow,end_column=invcol)
+                                ws.merge_cells(start_row=startmergerow,start_column=int_col,end_row=endmergerow,end_column=int_col)
+                                ws.merge_cells(start_row=startmergerow,start_column=autocallcol,end_row=endmergerow,end_column=autocallcol)
+                                rows_iterate.append(startmergerow)
+                                
+                                # Sort cols     
+                                RefAsset_Ident = []
+                                
+                                for row in range(startmergerow, endmergerow+1):
+                                    RefAsset_Ident.append([(ws.cell(row=row, column =ref_asset_col).value),(ws.cell(row=row, column =ident_col).value)])
+    
+                                # Sort the list first by "Ident" (the int value) then by "Reference Asset" (alphabetically)
+                                RefAsset_Ident.sort(key=lambda x: (x[1],x[0]))
+                                
+                                for i in range(startmergerow, endmergerow+1):
+                                    ws.cell(row=i, column=ref_asset_col).value = RefAsset_Ident[i-startmergerow][0]
+                                    ws.cell(row=i, column=ident_col).value = RefAsset_Ident[i-startmergerow][1]
+    
+                                startmergerow = None
+                                rowcounter += 1
+                                
+                    # Merge "Ident" Column
+                    ident_iterate = []
+                    ident_counter = 2
+                    ident_start_merge_row = None
+                    for cell in ws[identcolletter]:
+                        if cell.row == 1:
+                            continue
+                        elif (cell.value is not None) & (cell.value != 1):
+                            if ws[rf'{cell.column_letter}{ident_counter}'].value == ws[rf'{cell.column_letter}{ident_counter+1}'].value:
+                                if ident_start_merge_row is not None:
+                                    ident_counter += 1
+                                    continue
+                                else:
+                                    ident_start_merge_row = ws[rf'{cell.column_letter}{ident_counter}'].row
+                                    ident_counter += 1                            
+                                    continue
+                            else:
+                                if ident_start_merge_row is None:
+                                    ident_start_merge_row = ws[rf'{cell.column_letter}{ident_counter}'].row
+                                ident_end_merge_row = ws[rf'{cell.column_letter}{ident_counter}'].row
+                                ws.merge_cells(start_row=ident_start_merge_row,start_column=identcol,end_row=ident_end_merge_row,end_column=identcol)
+                                rows_iterate.append(ident_start_merge_row)
+                                ident_start_merge_row = None
+                                ident_counter += 1
+            
+                    # Change the font color to blue or red
+                    red = Font(color='FF0000', bold=True)
+                    blue = Font(color='0000FF')
+                    for d, h in zip(ws['AF'], ws['C']):
+                        if d.row in rows_iterate:
+                            if d.value == 'Valid IC Date':
+                                d.font = red
+                                h.font = blue
+            
+            # Usage:
+            wb = load_workbook(filename=fileloc)
+            process_worksheet(wb['CUSIP'], cusipdf, cusiprefassetlist, cusipvallist, cusipsettlelist, cusiplist, cusipnotional, cusipmemory, cusipinv, cusipident, cusipauto, cusiptpayingint, cusipinterest)
+            process_worksheet(wb['ISIN'], isindf, isinrefassetlist, isinvallist, isinsettlelist, isinlist, isinnotional, isinmemory, isininv, isinident, isinauto, isintpayingint, isininterest)
+
+            
+            
             wb = load_workbook(filename = fileloc)
 
             if cusipdf.empty == False:
